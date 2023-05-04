5cebdeb8294676e221b6973b37515c3ea1c02165
martinapiglio <martina.piglio@gmail.com>
2023-05-04 17:44:14 +0200

    cards completed

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
 <?php
 
-$food[] = new Food('Royal Canin Puppy', 'https://picsum.photos/200', 'Royal Canin', 'Lorem ipsum dolor sit amet consectetur', 'dog', 5, 'Croquettes');
-$food[] = new Food('Purin Pro Plan', 'https://picsum.photos/200', 'Purina', 'Lorem ipsum dolor sit amet consectetur', 'dog', 6, 'Wet');
-$food[] = new Food('Le Ghiottonerie', 'https://picsum.photos/200', 'Felix', 'Lorem ipsum dolor sit amet consectetur', 'cat', 4, 'Croquettes');
-$food[] = new Food('Next Cat Natural', 'https://picsum.photos/200', 'Next', 'Lorem ipsum dolor sit amet consectetur', 'cat', 5, 'Wet');
+$food[] = new Food('Royal Canin Puppy', 'https://picsum.photos/180/130', 'Royal Canin', 'Lorem ipsum dolor sit amet consectetur', 'dog', 5, 'Croquettes');
+$food[] = new Food('Purin Pro Plan', 'https://picsum.photos/180/130', 'Purina', 'Lorem ipsum dolor sit amet consectetur', 'dog', 6, 'Wet');
+$food[] = new Food('Le Ghiottonerie', 'https://picsum.photos/180/130', 'Felix', 'Lorem ipsum dolor sit amet consectetur', 'cat', 4, 'Croquettes');
+$food[] = new Food('Next Cat Natural', 'https://picsum.photos/180/130', 'Next', 'Lorem ipsum dolor sit amet consectetur', 'cat', 5, 'Wet');
 
-$toys[] = new Toy('Ball', 'https://picsum.photos/200', 'Smilla', 'Lorem ipsum dolor sit amet consectetur', 'dog', 5, 'Red', 'Plastic');
-$toys[] = new Toy('Bone', 'https://picsum.photos/200', 'Barkoo', 'Lorem ipsum dolor sit amet consectetur', 'dog', 3, 'White', 'Syntetic Beef');
-$toys[] = new Toy('Skein', 'https://picsum.photos/200', 'Schesir', 'Lorem ipsum dolor sit amet consectetur', 'cat', 6, 'Blue', 'Wool');
-$toys[] = new Toy('Scratcher', 'https://picsum.photos/200', 'Gourmet', 'Lorem ipsum dolor sit amet consectetur', 'cat', 10, 'Beige', 'Wicker');
+$toys[] = new Toy('Ball', 'https://picsum.photos/180/130', 'Smilla', 'Lorem ipsum dolor sit amet consectetur', 'dog', 5, 'Red', 'Plastic');
+$toys[] = new Toy('Bone', 'https://picsum.photos/180/130', 'Barkoo', 'Lorem ipsum dolor sit amet consectetur', 'dog', 3, 'White', 'Syntetic Beef');
+$toys[] = new Toy('Skein', 'https://picsum.photos/180/130', 'Schesir', 'Lorem ipsum dolor sit amet consectetur', 'cat', 6, 'Blue', 'Wool');
+$toys[] = new Toy('Scratcher', 'https://picsum.photos/180/130', 'Gourmet', 'Lorem ipsum dolor sit amet consectetur', 'cat', 10, 'Beige', 'Wicker');
 
-$medicine[] = new Medicine('Soft Gras', 'https://picsum.photos/200', 'Purina', 'Lorem ipsum dolor sit amet consectetur', 'cat', 10, 'catnip', false);
-$medicine[] = new Medicine('Frontline', 'https://picsum.photos/200', 'eFarma', 'Lorem ipsum dolor sit amet consectetur', 'cat', 9, 'flea repellent', false);
+$medicine[] = new Medicine('Soft Gras', 'https://picsum.photos/180/130', 'Purina', 'Lorem ipsum dolor sit amet consectetur', 'cat', 10, 'catnip', false);
+$medicine[] = new Medicine('Frontline', 'https://picsum.photos/180/130', 'eFarma', 'Lorem ipsum dolor sit amet consectetur', 'cat', 9, 'flea repellent', false);
 
 ?>
--- index.php
+++ index.php
@@ -15,9 +15,102 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP 2</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
 </head>
 <body>
 
+    <div class="container py-3">
 
+        <h1 class="p-2">Food</h1>
+
+        <div class="d-flex d-row justify-content-between">
+
+            <?php
+                foreach ($food as $foodProd) {
+            ?>
+
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="<?= $foodProd->img ?>" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title"><?= $foodProd->name ?></h5>
+                <p><strong><i><?= $foodProd->brand ?></i></strong></p>
+                <p><?= $foodProd->category ?></p>
+                <p class="card-text"> <small><?= $foodProd->type ?></small> </p>
+                <p class="card-text"><?= $foodProd->description ?></p>
+                <p class="card-text"><?= $foodProd->price ?> €</p>
+                
+                <a href="#" class="btn btn-dark">Add to Chart</a>
+            </div>
+            </div>
+                
+            <?php
+                }
+            ?>
+
+        </div>
+
+        <h1 class="p-2">Toys</h1>
+
+        <div class="d-flex d-row justify-content-between">
+
+            <?php
+                foreach ($toys as $toy) {
+            ?>
+
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="<?= $toy->img ?>" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title"><?= $toy->name ?></h5>
+                <p><strong><i><?= $toy->brand ?></i></strong></p>
+                <p><?= $toy->category ?></p>
+                <p> <strong>Color:</strong> <?= $toy->color ?> </p>
+                <p> <strong>Material:</strong> <?= $toy->material ?> </p>
+                <p class="card-text"><?= $toy->description ?></p>
+                <p class="card-text"><?= $toy->price ?> €</p>
+                
+                <a href="#" class="btn btn-dark">Add to Chart</a>
+            </div>
+            </div>
+                
+            <?php
+                }
+            ?>
+
+        </div>
+
+        <h1 class="p-2">Medicine</h1>
+
+        <div class="d-flex d-row gap-5">
+
+            <?php
+                foreach ($medicine as $medProd) {
+            ?>
+
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="<?= $medProd->img ?>" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title"><?= $medProd->name ?></h5>
+                <p><strong><i><?= $medProd->brand ?></i></strong></p>
+                <p><?= $medProd->category ?></p>
+                <p> <strong>Type:</strong> <?= $medProd->type ?> </p>
+                <p class="card-text"><?= $medProd->description ?></p>
+                <p class="card-text"><?= $medProd->price ?> €</p>
+                
+                <a href="#" class="btn btn-dark">Add to Chart</a>
+            </div>
+            </div>
+                
+            <?php
+                }
+            ?>
+
+        </div>
+
+    </div>
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
